dae6e9a29d24f87a676c9ce5a1ee024e2bdf84cb
Sergey Pegas <webpegasp@yandex.ru>
2024-01-01 15:32:04 +0300

    Спринт 7::  add 3

--- src/components/api.js
+++ src/components/api.js
@@ -6,22 +6,14 @@ const config = {
   },
 };
 
-/**
- * Базовая реализация GET запроса
- * @param { string } uri частичный путь после базового адреса
- */
+
 function get(uri) {
   return fetch(config.baseUrl + uri, {
     headers: config.headers,
   }).then(handleResponse);
 }
 
-/**
- * Базовая реализация POST запроса
- * @param { string } uri частичный путь после базового адреса
- * @param { object } data данные, передаваемые на сервер
- * @param { string } method HTTP метод запроса
- */
+
 function post(uri, data, method = "POST") {
   return fetch(config.baseUrl + uri, {
     method,
@@ -30,12 +22,7 @@ function post(uri, data, method = "POST") {
   }).then(handleResponse);
 }
 
-/**
- * Обработчик ошибок запроса
- * @param { Response } response объект с ответом сервера до загрузки данных
- * @return { Promise } в then всегда будет результат
- * @reject { status, error } в catch всегда будет ошибка
- */
+
 const handleResponse = (response) => {
   if (response.ok) {
     return response.json();
@@ -44,21 +31,12 @@ const handleResponse = (response) => {
   }
 };
 
-/**
- * Удаление карточки с сервера
- * @param { string } cardId
- * @returns { Promise }
- */
+
 export function deleteCardRequest(cardId) {
   return post(`/cards/${cardId}`, {}, "DELETE");
 }
 
-/**
- * Добавление новой карточки на сервер
- * @param { string } name
- * @param { string } link
- * @returns { Promise }
- */
+
 export function addCardRequest(cardName, cardLink) {
   return post("/cards", {
     name: cardName,
@@ -66,12 +44,7 @@ export function addCardRequest(cardName, cardLink) {
   });
 }
 
-/**
- * Изменение информации профиля
- * @param { string } inputName
- * @param { string } inputDescription
- * @returns { Promise }
- */
+
 export function changeProfileInfoRequest(profileName, profileDescription) {
   return post(
     "/users/me",
@@ -83,11 +56,7 @@ export function changeProfileInfoRequest(profileName, profileDescription) {
   );
 }
 
-/**
- * Отправка нового изображение аватара на сервер
- * @param { string } inputLink
- * @returns { Promise }
- */
+
 export function changeAvatarRequest(inputLink) {
   return post(
     "/users/me/avatar",
@@ -98,36 +67,22 @@ export function changeAvatarRequest(inputLink) {
   );
 }
 
-/**
- * Запрос информации о моем профиле
- * @returns { Promise }
- */
+
 export function getMyInfoRequest() {
   return get("/users/me");
 }
 
-/**
- * Запрос карточек с сервера
- * @returns { Promise }
- */
+
 export function getCardsRequest() {
   return get("/cards");
 }
 
-/**
- * Удаление лайка карточки с сервера
- * @param { object } card
- * @returns { Promise }
- */
+
 export function deleteLikeRequest(card) {
   return post(`/cards/likes/${card["_id"]}`, {}, "DELETE");
 }
 
-/**
- * Добавление лайка карточки с сервера
- * @param { object } card
- * @returns { Promise }
- */
+
 export function addLikeRequest(card) {
   return post(`/cards/likes/${card["_id"]}`, {}, "PUT");
 }
--- src/components/validation.js
+++ src/components/validation.js
@@ -1,15 +1,3 @@
-/* 
-    const validationConfig = {
-        formSelector: '.popup__form',
-        inputSelector: '.popup__input',
-        submitButtonSelector: '.popup__button',
-        inactiveButtonClass: 'popup__button_disabled',
-        inputErrorClass: 'popup__input_type_error',
-        errorClass: 'popup__error_visible'
-    }
-*/
-
-// Добавление классов ошибок валидации
 function showInputError(
   formElement,
   inputElement,
@@ -23,7 +11,7 @@ function showInputError(
   errorElement.textContent = errorMessage;
 }
 
-// Удаление классов ошибок валидации
+
 function hideInputError(formElement, inputElement, validationConfig) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
 
@@ -32,11 +20,8 @@ function hideInputError(formElement, inputElement, validationConfig) {
   errorElement.textContent = "";
 }
 
-// Проверка на валидность отдельного input
-function isValid(formElement, inputElement, validationConfig) {
-  // Добавление кастомного сообщения ошибки
-  // при использовании pattern
 
+function isValid(formElement, inputElement, validationConfig) {
   if (inputElement.validity.patternMismatch) {
     inputElement.setCustomValidity(inputElement.dataset.errorMessage);
   } else {
@@ -55,7 +40,7 @@ function isValid(formElement, inputElement, validationConfig) {
   }
 }
 
-// Добавление слушателя ввода на каждый input формы
+
 function setEventListeners(formElement, validationConfig) {
   const inputList = Array.from(
     formElement.querySelectorAll(validationConfig.inputSelector)
@@ -74,7 +59,7 @@ function setEventListeners(formElement, validationConfig) {
   });
 }
 
-// Добавление setEventListeners на каждую форму
+
 export function enableValidation(validationConfig) {
   const formList = Array.from(
     document.querySelectorAll(validationConfig.formSelector)
@@ -84,16 +69,14 @@ export function enableValidation(validationConfig) {
   });
 }
 
-// Проверка валидности каждого input в форме
-// для валидации всех полей
+
 function hasInvalidInput(inputList) {
   return inputList.some((item) => {
     return !item.validity.valid;
   });
 }
 
-// Добавление и удаление класса disabled для кнопки формы
-// при проверке валидации всех полей формы
+
 function toogleButtonState(inputList, buttonElement, validationConfig) {
   if (hasInvalidInput(inputList)) {
     buttonElement.disabled = true;
@@ -104,11 +87,7 @@ function toogleButtonState(inputList, buttonElement, validationConfig) {
   }
 }
 
-/**
- * Удаляет классы ошибок валидации при повторном открытии попапа с формой
- * (если были ошибки валидации и попап закрыли - при повторном открытии
- * нужно очистить ошибки валидации и очистить форму)
- */
+
 export function clearValidation(profileForm, validationConfig) {
   const buttonElement = profileForm.querySelector(
     validationConfig.submitButtonSelector
